f07ca3b593fbd7b0aadd795a58faee2d270937ec
Kamil Parzych <kamil.parzych@pl.bosch.com>
2018-09-10 16:53:13 +0200

    slider implementation

--- src/Kontrolery/MenuContr.java
+++ src/Kontrolery/MenuContr.java
@@ -3,6 +3,7 @@ package Kontrolery;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
@@ -46,6 +47,21 @@ public class MenuContr {
         Platform.exit();
     }
 
+    @FXML
+    public void onSliderBtn() throws IOException {
+        System.out.println("sliderBtn pressed");
+        FXMLLoader optLoader = new FXMLLoader(this.getClass().getResource("/FXML/slider.fxml"));
+        AnchorPane anchorPane = null;
+        try {
+            anchorPane = optLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        SliderController sliderController = optLoader.getController();
+        sliderController.setMainContr(mainContr);
+        mainContr.setScreen(anchorPane);
+    }
 
     public void setMainContr(MainContr mainContr) {
         this.mainContr = mainContr;
